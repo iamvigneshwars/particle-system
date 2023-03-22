dee165a8d68b435e086e8719d536529a00ef2203
iamvigneshwars <iamvigneshwars@gmail.com>
2023-03-22 22:50:15 +0000

    vector field

--- particle_sys2.py
+++ particle_sys2.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 import sys
 
 pygame.init()
@@ -9,6 +10,9 @@ FPS = 60
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
+font = pygame.font.Font(None, 15)
+
+target = (450, 230)
 
 class Particle:
     def __init__(self, x, y):
@@ -17,8 +21,18 @@ class Particle:
         self.xv = random.uniform(-2, 2)
         self.yv = random.uniform(-2, 2)
         self.color = (random.randint(0, 255), random.randint(0, 255),random.randint(0, 225))
+        self.life = 120
 
     def move(self):
+
+        dx = target[0] - self.x
+        dy = target[1] - self.y
+        distance = math.sqrt(dx**2 + dy**2)
+
+        if distance > 0:
+            self.xv += dx / distance * 0.2
+            self.yv += dy / distance * 0.2
+
         self.x += self.xv
         self.y += self.yv 
 
@@ -27,6 +41,10 @@ class Particle:
 
         self.xv *= 0.999
         self.yv *= 0.999
+        # self.life -= 1
+
+        if distance < 50: 
+            self.life = 0
 
     def draw(self):
         pygame.draw.circle(screen, self.color, (max(0, int(self.x)), int(self.y)), 5)
@@ -44,8 +62,8 @@ while True:
             sys.exit()
 
 
-    # if pygame.mouse.get_pressed()[0]:
-    #     particles.append(Particle(*pygame.mouse.get_pos()))
+    particles = [part for part in particles if part.life > 0]
+
     x_pos = random.randint(0, 600)
     y_pos = random.randint(0, 400)
     particles.append(Particle(x_pos, y_pos))
@@ -55,6 +73,11 @@ while True:
     for part in particles:
         part.move()
         part.draw()
+
+    target_surf = pygame.draw.circle(screen, 'Red', target, 50)
+
+    part_count = font.render(f"Count: {len(particles)}", False, 'White')
+    screen.blit(part_count, (20, 20))
 
     clock.tick(60)
     pygame.display.update()
