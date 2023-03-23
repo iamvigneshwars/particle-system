361d8d43b7607c72f0b4207b741f3a5da29cbca5
iamvigneshwars <iamvigneshwars@gmail.com>
2023-03-23 11:46:59 +0000

    rect particles

--- particle_sys4.py
+++ particle_sys4.py
@@ -1,30 +1,68 @@
 import pygame
 import math
 import random
+import sys
 
 pygame.init()
 
 size = (600, 600)
 fps = 60
-active = True
 
 screen = pygame.display.set_mode(size)
 clock  = pygame.time.Clock()
 
-while active:
+platform_surf = pygame.Surface((600, 100))
+platform_surf.fill('Red')
+platform_rect = platform_surf.get_rect(midbottom = (400, 400))
 
+class Particle:
+    def __init__(self, x, y):
+        self.x = max(0, min(x, size[0]))
+        self.y = max(0, min(y, size[1]))
+        self.vx = random.uniform(-2, 2)
+        self.vy = random.uniform(-2, 2)
+        self.surf = pygame.Surface((10, 10))
+        self.surf.fill((0, 0, random.randint(0, 255)))
+        self.ret = self.surf.get_rect(center=(self.x, self.y))
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x < 0 or self.x > size[0] : self.vx *= -1
+        if self.y < 0 or self.y > size[1] : self.vy *= -1
+
+        self.vx *= 0.99
+        self.vy *= 0.99
+
+
+    def draw(self):
+        self.ret.center = (self.x, self.y)
+        screen.blit(self.surf, self.ret)
+
+
+particles = []
+
+while True:
     for event in pygame.event.get():
-
         # Check for exit conditions
         if (event.type == pygame.QUIT):
-            active = False
+            sys.exit()
         if (event.type == pygame.KEYDOWN):
             if event.key == pygame.K_ESCAPE:
-                active = False
+                sys.exit()
 
-    screen.fill("Grey")
+    screen.fill('Grey')
+
+    if pygame.mouse.get_pressed()[0]:
+        particles.append(Particle(*pygame.mouse.get_pos()))
+
+    for particle in particles:
+        particle.move()
+        particle.draw()
+
+
     pygame.display.update()
     clock.tick(fps)
 
-pygame.quit()
 
